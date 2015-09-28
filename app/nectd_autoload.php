052e7ddb64403d459d943bcd1485915cf3da2582
Hoang C. Huynh <hoang@nectd.com>
2015-09-28 05:00:21 +0200

    Maxart offline fix commit, reslashing urls

--- app/nectd_autoload.php
+++ app/nectd_autoload.php
@@ -3,7 +3,7 @@ return call_user_func(function () {
     $includeRoot = __DIR__ . '/../src/Nectd/';
     spl_autoload_register(function($class) use ($includeRoot) {
         if (strpos($class, 'Nectd\\') === 0) {
-            $file = $includeRoot . substr($class, 6) . '.php';
+            $file = $includeRoot . str_replace('\\', '/', substr($class, 6)) . '.php';
             if (file_exists($file)) {
                 include $file;
             }

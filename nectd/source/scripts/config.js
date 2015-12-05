018b910e86261669bedf530295d45c88b9782a1c
Massimo Artizzu <maxart.x@gmail.com>
2015-12-05 23:59:52 +0100

    Object.assign

--- nectd/source/scripts/config.js
+++ nectd/source/scripts/config.js
@@ -1,3 +1,6 @@
 import promise from "es6-promise";
+import assign from "object-assign";
+
+if (!Object.assign) Object.assign = assign;
 
 export const env = /^(?:localhost|dev)\b/.test(location.hostname) ? "dev" : "prod";
